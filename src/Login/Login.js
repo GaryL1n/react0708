ca71152b6b67f0f260f3ac1a6c02a3c03523a1b5
GaryL1n <106055223+GaryL1n@users.noreply.github.com>
2022-07-15 09:02:12 +0800

    0715

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -97,7 +97,7 @@ const Login = () => {
                                             />
                                         </div>
                                     </div>
-                                    <div className="col-12 d-flex justify-content-around mt-4">
+                                    <div className="col-12 d-flex justify-content-around mt-4 FbLineGoogle">
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 488 512"

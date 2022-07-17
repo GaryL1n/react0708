74f25c96514cb8652e642793c6cd9fcc3d8b4a1d
Gary <garylin0969@gmail.com>
2022-07-17 18:50:05 +0800

    0717

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import RefsForm from './0715/12.refs與id的比較範例/ex-0715-6/RefsForm';
 import './App.css';
+import SwiperTry from './Components/Carousel/SwiperTry';
 
 const App = () => {
     return (
         <>
-            <RefsForm />
+            <SwiperTry />
         </>
     );
 };
